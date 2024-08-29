36b5aa3c81cd830cfe81e88c1c3932a70d7c4923
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-08-29 21:39:45 +0200

    [ADD] signup form, authentication OK, missing email verification, homepage in progress

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -1,7 +1,62 @@
+import { signInWithEmailAndPassword, getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+// import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+function showMessage(message, divId)
+{
+  const messageDiv = document.getElementById(divId);
+  messageDiv.innerHTML = message;
+  messageDiv.style.display = 'block';
+  messageDiv.style.opacity = 1;
+  setTimeout(() => {
+    messageDiv.style.opacity = 0;
+  }, 2000);
+  
+  if (message !== "Successfully logged in")
+  {
+    setTimeout(() => {
+      messageDiv.style.display = "none"; // Hide the message after fade-out
+    }, 3000); // Delay for 6 seconds (1 second after the opacity transition)
+  }
+}
+
 const signinForm = document.getElementById('submitSignin');
 
 signinForm.addEventListener('click', (e) => {
 	e.preventDefault();
-	
-	console.log('Form submitted');
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	const auth = getAuth();
+
+	signInWithEmailAndPassword(auth, email, password)
+	.then((cred) => {
+		showMessage('Successfully logged in', 'signinMessage');
+		const userData = {
+			email: email,
+			uid: cred.user.uid,
+		};
+		localStorage.setItem('loggedUser', userData.uid);
+		setTimeout(() => {
+			window.location.href = '/homepage';
+		}, 2000);
+	}).catch((err) => {
+		const errorCode = err.code;
+		if (errorCode === 'auth/invalid-credential') {
+			showMessage('Invalid email or password', 'signinMessage');
+		}
+		else {
+			showMessage('Account does not exist', 'signinMessage');
+		}
+	});
+});
+
+const togglePassword = document.getElementById('togglePassword');
+
+togglePassword.addEventListener('click', () => {
+  // Toggle the type attribute using getAttribute() and setAttribute()
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  
+  // Toggle the eye / eye slash icon
+  togglePassword.innerHTML = type === 'password' ? '<i class="fas fa-eye"></i>' : '<i class="fas fa-eye-slash"></i>';
 });
--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,12 +1,31 @@
 import { createUserWithEmailAndPassword, getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-const auth = getAuth();
+
+function showMessage(message, divId)
+{
+  const messageDiv = document.getElementById(divId);
+  messageDiv.innerHTML = message;
+  messageDiv.style.display = 'block';
+  messageDiv.style.opacity = 1;
+  setTimeout(() => {
+    messageDiv.style.opacity = 0;
+  }, 2000);
+  
+  if (message !== "User created successfully")
+  {
+    setTimeout(() => {
+      messageDiv.style.display = "none"; // Hide the message after fade-out
+    }, 3000); // Delay for 6 seconds (1 second after the opacity transition)
+  }
+}
 
 const signupForm = document.getElementById("submitSignup");
-
 signupForm.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Form submitted");
+
+  const auth = getAuth();
+  const db = getFirestore();
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -14,8 +33,31 @@ signupForm.addEventListener("click", (e) => {
   createUserWithEmailAndPassword(auth, email, password)
     .then((cred) => {
       console.log("User created : ", cred.user);
+      const userData = {
+        email: email,
+        uid: cred.user.uid,
+      };
+      showMessage("User created successfully", "signupMessage")
+      const docRef = doc(db, "users", userData.uid);
+      setDoc(docRef, userData)
+      .then(() => {
+        setTimeout(() => {
+          window.location.href = "/signin";
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log("error writting document: ", error);
+      });
     })
     .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/email-already-in-use") {
+        showMessage("Email already Exists !", "signupMessage");
+      }
+      else
+      {
+        showMessage("Error creating user !", "signupMessage");
+      }
       console.log(error.message);
     });
 });
@@ -30,3 +72,5 @@ togglePassword.addEventListener('click', () => {
   // Toggle the eye / eye slash icon
   togglePassword.innerHTML = type === 'password' ? '<i class="fas fa-eye"></i>' : '<i class="fas fa-eye-slash"></i>';
 });
+
+export { showMessage };
