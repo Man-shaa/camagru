7763cceda01464510f83954ae5353f3f9779d057
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-08-29 22:40:34 +0200

    [ADD] logout button

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,12 +1,19 @@
-import { onAuthStateChanged, getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { onAuthStateChanged, getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const auth = getAuth();
 const db = getFirestore();
 
+const signinBtnContainer = document.getElementById('signin-btn-container');
+const logoutBtnContainer = document.getElementById('logout-btn-container');
+
 onAuthStateChanged(auth, (user) => {
 	const loggedUser = localStorage.getItem('loggedUser');
 	if (loggedUser) {
+		// show logout button
+		signinBtnContainer.style.display = 'none';
+    logoutBtnContainer.style.display = 'block';
+
 		const docRef = doc(db, 'users', loggedUser);
 		getDoc(docRef)
 		.then((docSnap) => {
@@ -27,6 +34,24 @@ onAuthStateChanged(auth, (user) => {
 	}
 	else
 	{
+		// No user is logged in, show the signin button
+    signinBtnContainer.style.display = 'block';
+    logoutBtnContainer.style.display = 'none';
+
 		console.log('user Id not found in local storage');
 	}
+});
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+
+	localStorage.removeItem('loggedUser');
+	signOut(auth)
+	.then(() => {
+		window.location.href = '/signin';
+	})
+	.catch((error) => {
+		console.log('Error signing out:', error);
+	});
 });
