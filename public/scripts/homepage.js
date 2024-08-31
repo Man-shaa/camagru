ab14428f3e0b3579a6922c89e2a4988b31514d82
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-08-31 19:40:43 +0200

    [ADD] display images (need sacling and pagination), move image data in new "images" collection

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged, getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { getFirestore, addDoc, collection, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getFirestore, getDocs, addDoc, collection, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
 
 const auth = getAuth();
@@ -32,14 +32,26 @@ document.getElementById('uploadButton').addEventListener('click', async () => {
 			}
 		};
 
+		// upload on firebase storage
 		uploadBytes(imageStorageRef, file, metadata)
 		.then((snapshot) => {
 			console.log(`Uploaded ${file.name}!`);
 
-			addDoc(collection(db, 'users', currentUser.uid, 'images'), {
-				imageUrl: uniqueFileName,
-				fileName: file.name,
-		});
+			getDownloadURL(snapshot.ref)
+			.then((url) => {;
+            console.log('File available at', url);
+
+			// add image data to firestore database
+				addDoc(collection(db, 'images'), {
+					UniqueImageName: uniqueFileName,
+					imageUrl : url,
+					fileName: file.name,
+					userId: currentUser.uid
+				});
+			})
+			.catch((error) => {
+				console.log("Error getting download URL: ", error);
+			});
 		})
 		.catch((error) => {
 			console.log("Error uploading file: ", error);
@@ -51,40 +63,37 @@ document.getElementById('uploadButton').addEventListener('click', async () => {
   }
 });
 
-
-// getDownloadURL(ref(storage, 'images/Chapsounette.png'))
-// .then((url) => {
-// 	console.log("url : ", url);
-// 	// `url` is the download URL for 'images/stars.jpg'
-
-// 	// Or inserted into an <img> element
-// 	const img = document.getElementById('myimg');
-// 	img.setAttribute('src', url);
-// })
-// .catch((error) => {
-// 	// Handle any errors
-// });
-
 onAuthStateChanged(auth, (user) => {
 	console.log("user status : ", user);
 	if (user) {
     logoutBtnContainer.style.display = 'block';
 		currentUser = user;
 
-		// const docRef = doc(db, "users", user.uid);
-		// getDoc(docRef)
-		// .then((docSnap) => {
+		const imagesCollectionRef = collection(db, 'images');
 
-		// 	if (docSnap.exists()) {
-		// 		console.log("Document data:", docSnap.data());
-		// 	} else {
-		// 		// docSnap.data() will be undefined in this case
-		// 		console.log("No user found matching Uid");
-		// 	}
-		// })
-		// .catch((error) => {
-		// 	console.log("Error getting document:", error);
-		// });
+		getDocs(imagesCollectionRef)
+		.then((querySnapshot) => {
+
+			const imagesContainer = document.getElementById('imagesContainer');
+			imagesContainer.innerHTML = '';
+
+			querySnapshot.forEach((doc) => {
+				const imageUrl = doc.data().imageUrl; // Get image URL from Firestore
+	
+				// Create an img element and set its src to the image URL
+				const imgElement = document.createElement('img');
+				imgElement.src = imageUrl;
+				imgElement.alt = 'User uploaded image';
+				imgElement.className = 'uploaded-image';
+	
+				// Append the image element to the container
+				imagesContainer.appendChild(imgElement);
+			});
+
+		})
+		.catch((error) => {
+			console.log("Error getting document:", error);
+		});
 	}
 	else
 	{
